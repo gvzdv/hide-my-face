5dfa0319086864853dec90107e92e3b449f33586
Mike Gvozdev <mv.gvozdev@gmail.com>
2024-06-24 12:19:53 +0200

    clean up test code

--- main.py
+++ main.py
@@ -1,52 +1,9 @@
-# import face_recognition
-# import replicate
-# import os
-# from dotenv import load_dotenv
-#
-# load_dotenv()
-# REPLICATE_API_KEY = os.getenv('REPLICATE_API_KEY')
-#
-#
-# def detect_faces(image_path):
-#     # Load the image
-#     img = face_recognition.load_image_file(image_path)
-#     # Detect faces
-#     face_locations = face_recognition.face_locations(img)
-#
-#     if len(face_locations) > 0:
-#         faces = True
-#     else:
-#         faces = False
-#
-#     return faces
-#
-#
-# def replace_face(image, swap):
-#     client = replicate.Client(api_token=REPLICATE_API_KEY)
-#     output = client.run(
-#         "yan-ops/face_swap:ad6bd82aaff9ffabee90890c7bfbb249e4433b7a9f0ebf27b39f927edbd9b129",
-#         input={"target_image": open(image, "rb"),
-#                "source_image": open(swap, "rb")},
-#     )
-#     print(output)
-#
-#
-# upload = 'ryan.jpeg'
-# replacement = "random_3.png"
-#
-# result = detect_faces(upload)
-#
-# if result:
-#     replace_face(upload, replacement)
-import io
-
 from flask import Flask, request, jsonify, send_from_directory, render_template
 import os
 from werkzeug.utils import secure_filename
 import face_recognition
 import replicate
 from dotenv import load_dotenv
-import requests
 
 load_dotenv()
 REPLICATE_API_KEY = os.getenv('REPLICATE_API_KEY')
@@ -57,10 +14,12 @@ app.config['UPLOAD_FOLDER'] = 'uploads'
 if not os.path.exists(app.config['UPLOAD_FOLDER']):
     os.makedirs(app.config['UPLOAD_FOLDER'])
 
+
 def detect_faces(image_path):
     img = face_recognition.load_image_file(image_path)
     face_locations = face_recognition.face_locations(img)
     return len(face_locations) > 0
+
 
 def replace_face(image, swap):
     client = replicate.Client(api_token=REPLICATE_API_KEY)
@@ -71,9 +30,11 @@ def replace_face(image, swap):
     response = output['image']
     return response
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -94,6 +55,7 @@ def upload_file():
 
         return jsonify({'face_detected': False, 'image_url': filepath})
 
+
 @app.route('/replace', methods=['POST'])
 def replace_image():
     data = request.get_json()
@@ -102,9 +64,11 @@ def replace_image():
     replaced_image = replace_face(image_url, swap_image)
     return jsonify({'image_url': replaced_image})
 
+
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
